323da916682a5e6cd3eb1102097d3a1c0d2bf609
Craig Rowe <craig@cargowire.net>
2011-05-10 15:03:21 +0100

    Added branch class for representation of reference information from pushes

---
+++ Cargowire.CIBridge/Branch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Cargowire.CIBridge
+{
+	public class Branch
+	{
+		public string Name { get; set; }
+		public bool IsMaster { get; set; }
+	}
+}
--- Cargowire.CIBridge/HookInfo.cs
+++ Cargowire.CIBridge/HookInfo.cs
@@ -3,6 +3,7 @@ namespace Cargowire.CIBridge
 {
 	public class HookInfo
 	{
+		public Branch Branch { get; set; }
 		public User User { get; set; }
 		public Repository Repository { get; set; }
 	}
